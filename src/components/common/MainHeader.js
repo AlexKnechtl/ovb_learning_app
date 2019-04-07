c26343c49c8491379c4efc9fb4124d032f8319ce
Alexander Knechtl <alex.knechtl@gmx.at>
2019-04-07 17:06:41 +0200

    PDF Icon Fix

--- src/components/common/MainHeader.js
+++ src/components/common/MainHeader.js
@@ -11,7 +11,7 @@ const MainHeader = (props) => {
             </View>
             <View style={[styles.linearLayout2, props.style]}>
                 <View style={styles.iconViewLayout}>
-                    <TouchableOpacity hidden onPress={props.optionsPress}>
+                    <TouchableOpacity hidden>
                         <View style={[styles.iconView, props.style2]}>
                             <Image style={[styles.pdfImageStyle, props.style2]} source={require('../../img/pdf_icon.png')} />
                         </View>
